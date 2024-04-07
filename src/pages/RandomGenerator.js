5b19adc3d379583ea587dfcdfcc22af4aaee4cc0
DivyanshiSaini <sainid@ufl.edu>
2024-04-07 03:05:02 -0400

    Format correct

--- src/pages/RandomGenerator.js
+++ src/pages/RandomGenerator.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import studySpotsData from "./studySpotData.json";
-import "./RandomGenerator.css"; // Import CSS file for styling
+import "./RandomGenerator.css"; 
 
 function StudySpotGenerator() {
-  const [randomSpot, setRandomSpot] = useState(null);
+  const [randomSpot, setRandomSpot] = useState();
 
   const generateRandomSpot = () => {
     const randomIndex = Math.floor(Math.random() * studySpotsData.length);
@@ -11,24 +11,26 @@ function StudySpotGenerator() {
   };
 
   return (
-    <div className="container">
-      <h1>Random Study Spot Generator</h1>
-      <button onClick={generateRandomSpot}>Generate Random Study Spot</button>
+    <div className="Home">
+      <h1>Study Spot Finder</h1>
+      <button onClick={generateRandomSpot}>Find Me A Study Spot</button>
+
+
       {randomSpot && (
-        <div className="spot-container">
-          {/* Left side with image */}
-          <div className="image-container">
-            <img src={`images/${randomSpot.image}`} alt={randomSpot.buildingName} />
-          </div>
-          {/* Right side with text */}
-          <div className="text-container">
-            <h2>{randomSpot.buildingName}</h2>
+                  
+    
+        <><div className="LeftSide">
+        <img src={`/images/${randomSpot.image}`} alt={randomSpot.buildingName} />
+        </div>
+        
+        <div className="rightSide">
+            <h2 className="rightSideTitle">{randomSpot.buildingName}</h2>
             <p><strong>Address:</strong> {randomSpot.address}</p>
             <p><strong>Study Booths Available:</strong> {randomSpot.studyBoothsAvailable}</p>
             <p><strong>Food Availability:</strong> {Array.isArray(randomSpot.foodAvailability) ? randomSpot.foodAvailability.join(", ") : randomSpot.foodAvailability}</p>
             <p><strong>Bus Routes:</strong> {randomSpot.busRoutes.join(", ")}</p>
-          </div>
-        </div>
+          </div></>
+
       )}
     </div>
   );
