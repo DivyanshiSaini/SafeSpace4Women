211d79d5860e923c502844bd5f8288218bead6bb
DivyanshiSaini <sainid@ufl.edu>
2024-04-07 04:10:58 -0400

    Website works

--- src/pages/RandomGenerator.js
+++ src/pages/RandomGenerator.js
@@ -12,25 +12,26 @@ function RandomGenerator() {
 
   return (
     <div className="Home">
-      <h1>Study Spot Finder</h1>
+       <h2 className="Title">{"Study Spot Finder"}</h2>
       <button onClick={generateRandomSpot}>Find Me A Study Spot</button>
 
-
       {randomSpot && (
-                  
-    
-        <><div className="LeftSide">
-        <img src={`/images/${randomSpot.image}`} alt={randomSpot.buildingName} />
-        </div>
-        
-        <div className="rightSide">
+        <div className="box">
+         
+          <div className="LeftSide">
+            <img src={`/images/${randomSpot.image}`} alt={randomSpot.buildingName} />
+          </div>
+
+          <div className="rightSide">
             <h2 className="rightSideTitle">{randomSpot.buildingName}</h2>
             <p><strong>Address:</strong> {randomSpot.address}</p>
-            <p><strong>Study Booths Available:</strong> {randomSpot.studyBoothsAvailable}</p>
+            <p><strong>Operating Hours:</strong> {randomSpot.operatinghours}</p>
             <p><strong>Food Availability:</strong> {Array.isArray(randomSpot.foodAvailability) ? randomSpot.foodAvailability.join(", ") : randomSpot.foodAvailability}</p>
             <p><strong>Bus Routes:</strong> {randomSpot.busRoutes.join(", ")}</p>
-          </div></>
+          </div>
 
+
+        </div>
       )}
     </div>
   );
